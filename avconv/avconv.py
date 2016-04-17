17788b020070584dc8d1c83514d25f820e50f4a9
Stefan Antoni <stefan@antoni.io>
2016-04-17 11:31:26 +0200

    adding avconv test

---
+++ avconv/avconv.py
@@ -0,0 +1,28 @@
+"""A python FFMPEG module built from sdpm."""
+from __future__ import unicode_literals
+from sidomo import Container
+
+
+def transcode_file(url):
+    """Any format --> 20000 Hz mono wav audio."""
+    with Container(
+        'suchkultur/avconv',
+        memory_limit_gb=2,
+        stdout=False
+    ) as c:
+        for line in c.run(
+            'bash -c \"\
+                wget -nv -O tmp.unconverted %s;\
+                avconv -i tmp.unconverted -f wav -acodec pcm_s16le -ac 1 -ar 20000 tmp.wav;\
+                cat tmp.wav\
+            \"\
+            ' % url
+        ):
+            yield line
+
+
+if __name__ == '__main__':
+    print("I'm gonna transcode an audio file and only print the stderr.")
+    url = 'http://www2.warwick.ac.uk/fac/soc/sociology/staff/sfuller/media/audio/9_minutes_on_epistemology.mp3'
+    for line in transcode_file(url):
+        print(line)
---
+++ ubuntutest.py
@@ -0,0 +1,18 @@
+from sidomo import Container
+
+
+def say_hello(to):
+    """Just say it."""
+    with Container(
+        'ubuntu',
+        stderr=False
+    ) as c:
+        for line in c.run(
+            'echo hello %s' % to
+        ):
+            yield line
+
+
+if __name__ == '__main__':
+    for line in say_hello("world"):
+        print(line)
